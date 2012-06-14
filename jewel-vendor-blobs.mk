b05d522a557a0da4ed30c43c1f610ffe839e5609
jared waters <kushdeck@gmail.com>
2012-06-14 14:48:14 -0700

    add missing prop for vid cam

--- jewel-vendor-blobs.mk
+++ jewel-vendor-blobs.mk
@@ -56,6 +56,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc/jewel/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
   vendor/htc/jewel/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
   vendor/htc/jewel/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
+  vendor/htc/jewel/proprietary/lib/libcam_oem_plugin.so:system/lib/libcam_oem_plugin.so \
   vendor/htc/jewel/proprietary/lib/libDxOAF.so:system/lib/libDxOAF.so \
   vendor/htc/jewel/proprietary/lib/libgemini.so:system/lib/libgemini.so \
   vendor/htc/jewel/proprietary/lib/libmmcamera_frameproc.so:system/lib/libmmcamera_frameproc.so \
